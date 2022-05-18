817b7821100aaadd1d91bb0f21db48781ba793ed
Amirkhan <dlordproudd@gmail.com>
2022-05-18 08:39:08 +0300

    init project

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT | 4000;
+
+app.use(express.static(path.join(__dirname, 'dist')))
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`)
+})
